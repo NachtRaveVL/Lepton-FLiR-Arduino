925b148ba7c727632ab5ab0f90e1276b442fc7c9
NachtRaveVL <nachtravevl@gmail.com>
2020-08-12 00:03:44 -0700

    Previous one was missing its initialization, thus wasn't correct. This one is likely to work better.

--- examples/ModuleInfo/ModuleInfo.ino
+++ examples/ModuleInfo/ModuleInfo.ino
@@ -18,6 +18,13 @@ LeptonFLiR flirController;
 void setup() {
     Serial.begin(115200);
 
+    Wire.begin();                       // Wire must be started first
+    Wire.setClock(400000);              // Supported baud rates are 100kHz, 400kHz, and 1000kHz
+    SPI.begin();                        // SPI must be started first as well
+
+    // Using 40x30 8bpp memory allocation mode and default celsius temperature mode
+    flirController.init(LeptonFLiR_ImageStorageMode_40x30_8bpp);
+
     flirController.printModuleInfo();
 }
 
